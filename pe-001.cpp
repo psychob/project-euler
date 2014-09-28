230af7315250ca309bf36be7795c2c9de84b844e
Andrzej Budzanowski <psychob.pl@gmail.com>
2014-09-28 14:29:53 +0200

    initial commit

---
+++ pe-001.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main()
+{
+ int sum = 0,
+     it  = 1000;
+
+ while ( it --> 0 )
+ {
+  if ( it % 3 == 0 || it % 5 == 0 )
+   sum += it;
+ }
+ 
+ std::cout << "anserw: " << sum << std::endl;
+ return 0;
+}
---
+++ premake4.lua
@@ -0,0 +1,10 @@
+solution "project euler"
+ configurations "basic"
+ 
+ targetdir "./build/"
+ location  "./projfiles"
+ 
+ project "pe-001"
+  kind     "ConsoleApp"
+  language "c++"
+  files  { "pe-001.cpp" }
