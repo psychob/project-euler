6febaee32bb3fd5cd6fa8c17c31ab9897e58800d
Andrzej Budzanowski <psychob.pl@gmail.com>
2014-11-20 13:33:25 +0100

    Visual będzie generował pliki assemblera przy kompilacji + formatowanie

--- premake4.lua
+++ premake4.lua
@@ -1,54 +1,57 @@
 solution "project euler"
  configurations { "debug", "release" }
- 
+
  targetdir "./build/"
  location  "./projfiles/"
- 
+
  configuration "debug"
   flags { "Symbols" }
   objdir "./build/obj/"
   targetsuffix "-d"
-  
+
  configuration "release"
   flags { "Optimize" }
   objdir "./build/obj/"
   targetsuffix "-r"
- 
+
+ configuration { "Windows", "vs2010" }
+  buildoptions { "/FAs" }
+
  project "pe-001"
   kind     "ConsoleApp"
   language "c++"
   files  { "pe-001.cpp" }
- 
+
  project "pe-002"
   kind     "ConsoleApp"
   language "c++"
   files  { "pe-002.cpp" }
- 
+
  project "pe-003"
   kind     "ConsoleApp"
   language "c++"
   files  { "pe-003.cpp" }
- 
+
  project "pe-005"
   kind     "ConsoleApp"
   language "c++"
   files  { "pe-005.cpp" }
- 
+
  project "pe-006"
   kind     "ConsoleApp"
   language "c++"
   files  { "pe-006.cpp" }
- 
+
  project "pe-007"
   kind     "ConsoleApp"
   language "c++"
   files  { "pe-007.cpp" }
- 
+
  project "pe-008"
   kind     "ConsoleApp"
   language "c++"
   files  { "pe-008.cpp" }
- 
+
  project "pe-010"
   kind     "ConsoleApp"
   language "c++"
